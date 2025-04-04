4239d0be528584d595b5e2dd467789a3a94bfec8
Sergio Inzunza <inxunxa@gmail.com>
2025-04-03 17:41:08 -0700

    Some changes

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 LOGIN_REDIRECT_URL = "home"
+
 LOGOUT_REDIRECT_URL = "login"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
