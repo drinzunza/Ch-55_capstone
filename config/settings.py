b1e5e7f1604d03d60cd40536265cb3a95bcb86fb
Sergio Inzunza <inxunxa@gmail.com>
2025-04-03 18:15:38 -0700

    Some changes

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+
 LOGIN_REDIRECT_URL = "home"
 
 LOGOUT_REDIRECT_URL = "login"
